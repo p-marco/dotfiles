2abfe7b584ea038d8b03e306c5942b6b6f41ac63
p-marco <marco.petolicchio@gmail.com>
2019-05-16 19:12:21 +0200

    gtk2

--- symlinkFiles.sh
+++ symlinkFiles.sh
@@ -7,6 +7,6 @@ ln -s -f ~/dotfiles/polybar/config               ~/.config/polybar/config
 ln -s -f ~/dotfiles/rofi/config                 ~/.config/rofi/config
 ln -s -f ~/dotfiles/rofi/side.rasi              ~/.config/rofi/side.rasi
 ln -s -f ~/dotfiles/.Xresources                 ~/.Xresources
-ln -s -f ~/dotfiles/summer.jpg                 ~/Pictures/summer.jpg
+cp ~/dotfiles/summer.jpg                 ~/Pictures/bg.jpg
 ln -s -f ~/dotfiles/gtk-3.0/settings.ini        ~/.config/gtk-3.0/settings.ini
-ln -s -f ~/dotfiles/.gtk-2.0                ~/.config/.gtk-2.0
+ln -s -f ~/dotfiles/.gtk-2.0                    ~/.gtk-2.0
