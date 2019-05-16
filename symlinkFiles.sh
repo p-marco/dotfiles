74017c4c6bec75592f96fc74e55a473e790613ce
p-marco <marco.petolicchio@gmail.com>
2019-05-16 18:27:13 +0200

    sh

---
+++ symlinkFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s -f ~/dotfiles/Code/User/settings.json     ~/.config/Code/User/settings.json
+ln -s -f ~/dotfiles/dunst/dunstrc               ~/.config/dunst/dunstrc
+ln -s -f ~/dotfiles/i3/config                   ~/.config/i3/config
+ln -s -f ~/dotfiles/neofetch/config             ~/.config/neofetch/config
+ln -s -f ~/dotfiles/polybar/config               ~/.config/polybar/config
+ln -s -f ~/dotfiles/rofi/config                 ~/.config/rofi/config
+ln -s -f ~/dotfiles/rofi/side.rasi              ~/.config/rofi/side.rasi
