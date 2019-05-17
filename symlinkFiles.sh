d0bd33ef3c3197b4a39ead741d580f34d28c0bad
p-marco <marco.petolicchio@gmail.com>
2019-05-17 02:09:08 +0200

    b

--- symlinkFiles.sh
+++ symlinkFiles.sh
@@ -12,14 +12,14 @@ mkdir -p ~/.config/gtk-3.0
 
 # Symlink files
 
-ln -s -f ./Code/User/settings.json     ~/.config/Code/User/settings.json
-ln -s -f ./dunst/dunstrc               ~/.config/dunst/dunstrc
-ln -s -f ./i3/config                   ~/.config/i3/config
-ln -s -f ./neofetch/config             ~/.config/neofetch/config
-ln -s -f ./polybar/config              ~/.config/polybar/config
-ln -s -f ./rofi/config                 ~/.config/rofi/config
-ln -s -f ./rofi/side.rasi              ~/.config/rofi/side.rasi
-ln -s -f ./.Xresources                 ~/.Xresources
-ln -s -f ./gtk-3.0/settings.ini        ~/.config/gtk-3.0/settings.ini
-ln -s -f ./.gtk-2.0                    ~/.gtk-2.0
+ln -s -f ~/Vendors/dotfiles/Code/User/settings.json     ~/.config/Code/User/settings.json
+ln -s -f ~/Vendors/dotfiles/dunst/dunstrc               ~/.config/dunst/dunstrc
+ln -s -f ~/Vendors/dotfiles/i3/config                   ~/.config/i3/config
+ln -s -f ~/Vendors/dotfiles/neofetch/config             ~/.config/neofetch/config
+ln -s -f ~/Vendors/dotfiles/polybar/config              ~/.config/polybar/config
+ln -s -f ~/Vendors/dotfiles/rofi/config                 ~/.config/rofi/config
+ln -s -f ~/Vendors/dotfiles/rofi/side.rasi              ~/.config/rofi/side.rasi
+ln -s -f ~/Vendors/dotfiles/.Xresources                 ~/.Xresources
+ln -s -f ~/Vendors/dotfiles/gtk-3.0/settings.ini        ~/.config/gtk-3.0/settings.ini
+ln -s -f ~/Vendors/dotfiles/.gtk-2.0                    ~/.gtk-2.0
 cp ~/Vendors/dotfiles/summer.jpg                         ~/Library/Pictures/bg.jpg
