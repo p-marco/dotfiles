93354d365f648ff9ea3e6d2783f30c20af6baeaa
p-marco <marco.petolicchio@gmail.com>
2019-05-18 16:58:58 +0200

    add homeBootstrap and vscodeExtensions

---
+++ homeBootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Create home directories
+cd ~
+mkdir -p Documents
+mkdir -p Documents/Projects
+mkdir -p Documents/Research
+mkdir -p Documents/Various
+mkdir -p Downloads
+mkdir -p Library
+mkdir -p Library/Music
+mkdir -p Library/Readings
+mkdir -p Library/Videos
+mkdir -p Library/Pictures
+mkdir -p Vendors
---
+++ vscodeExtensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Install VSCode Extensions
+cd ~
+
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension Ikuyadeu.r
+code --install-extension James-Yu.latex-workshop
+code --install-extension Mikhail-Arkhipov.r
+code --install-extension notZaki.pandocciter
+code --install-extension tomoki1207.pdf
